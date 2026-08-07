1658d0f5fd675f87d4f6a3c5316469309da7113d
khsfashi <55976865+khsfashi@users.noreply.github.com>
2026-08-08 00:10:09 +0900

    Cover renderer headless isolation

---
+++ tests/render/RendererTests.cpp
@@ -0,0 +1,35 @@
+#include <trace2d/platform/Platform.hpp>
+#include <trace2d/render/Renderer.hpp>
+
+#include <gtest/gtest.h>
+
+#include <stdexcept>
+
+namespace
+{
+TEST(RendererTests, HeadlessPlatformHasNoWindowIdentifier)
+{
+    trace2d::platform::PlatformConfig platformConfig{};
+    platformConfig.mode = trace2d::platform::StartupMode::Headless;
+
+    const trace2d::platform::Platform platform{platformConfig};
+
+    EXPECT_FALSE(platform.HasWindow());
+    EXPECT_EQ(platform.WindowIdValue(), trace2d::platform::InvalidWindowId);
+}
+
+TEST(RendererTests, RendererRejectsHeadlessPlatformBeforeGpuInitialization)
+{
+    trace2d::platform::PlatformConfig platformConfig{};
+    platformConfig.mode = trace2d::platform::StartupMode::Headless;
+
+    const trace2d::platform::Platform platform{platformConfig};
+    const trace2d::render::RendererConfig rendererConfig{};
+
+    EXPECT_THROW(
+        {
+            const trace2d::render::Renderer renderer{rendererConfig, platform};
+        },
+        std::invalid_argument);
+}
+} // namespace
