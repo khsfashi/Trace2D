3f5e663215861fbe4bb38e0101f69b56da7ab332
khsfashi <55976865+khsfashi@users.noreply.github.com>
2026-08-16 13:05:51 +0900

    refactor(cli): split authoring kind commands

---
+++ tools/trace2d/AuthoringCommands.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace trace2d::tools
+{
+int RunSpriteAuthorCommand(int argc, char* argv[]);
+int RunParticleAuthorCommand(int argc, char* argv[]);
+} // namespace trace2d::tools
