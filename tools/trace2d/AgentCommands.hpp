b5bf9944600c2739699a86886acb5cfc049ed798
khsfashi <55976865+khsfashi@users.noreply.github.com>
2026-08-16 12:45:51 +0900

    feat(cli): declare sprite authoring command

--- tools/trace2d/AgentCommands.hpp
+++ tools/trace2d/AgentCommands.hpp
@@ -4,4 +4,5 @@ namespace trace2d::tools
 {
 int RunInspectCommand(int argc, char* argv[]);
 int RunQueryCommand(int argc, char* argv[]);
+int RunAuthorCommand(int argc, char* argv[]);
 } // namespace trace2d::tools
